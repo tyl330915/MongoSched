28d46b7e0883b7626a9e1a48558e39154e0e038b
tyl330915 <66554645+tyl330915@users.noreply.github.com>
2023-11-26 11:56:53 -0500

    Initial setup of SchedMongo

---
+++ server.js
@@ -0,0 +1,13 @@
+console.log("Hello from the server!");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Schedulator Mongo listening on port ${port}`)
+})
